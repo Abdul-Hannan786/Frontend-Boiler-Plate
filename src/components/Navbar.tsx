5dfb22c122f5b6ea75f72a3b6882c5e01c69e226
Abdul-Hannan786 <abdulhannanhere@gmail.com>
2025-01-27 00:22:14 +0500

    frontend update

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -22,8 +22,8 @@ const Navbar = () => {
       <nav className="backdrop-blur w-full border-gray-200 px-4 lg:px-6 py-2.5">
         <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
           <a href="https://flowbite.com" className="flex items-center">
-            <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
-              Flowbite
+            <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
+              Microfinance
             </span>
           </a>
           <div className="flex items-center lg:order-2">
